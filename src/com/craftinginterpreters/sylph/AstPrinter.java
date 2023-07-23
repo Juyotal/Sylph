ddec0cf4151e41b859258142eb993ce90f7aafcc
Juyotal <cesarkamga3@gmail.com>
2023-07-23 15:14:05 +0200

    Scanner and Parser running

--- src/com/craftinginterpreters/sylph/AstPrinter.java
+++ src/com/craftinginterpreters/sylph/AstPrinter.java
@@ -16,17 +16,6 @@ class AstPrinter implements Expr.Visitor<String> {
         return builder.toString();
     }
 
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)),
-                new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Grouping(
-                        new Expr.Literal(45.67)));
-        System.out.println(new AstPrinter().print(expression));
-    }
-
     @Override
     public String visitBinaryExpr(Expr.Binary expr) {
         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
